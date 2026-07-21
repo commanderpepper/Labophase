2d644d9e1b57b603ab7f6ad9f45729861307e5a5
humza <ahmadhumza7@gmail.com>
2026-07-21 12:03:55 -0700

    removed TournamentResultInterpreter

--- app/src/main/java/commanderpepper/labophase/di/AppModule.kt
+++ app/src/main/java/commanderpepper/labophase/di/AppModule.kt
@@ -8,14 +8,12 @@ import commanderpepper.labophase.data.SettingsRepositoryImpl
 import commanderpepper.labophase.data.settingsDataStore
 import commanderpepper.labophase.logic.LeaderOrderDecider
 import commanderpepper.labophase.logic.LeaderOrderDeciderImpl
-import commanderpepper.labophase.logic.TournamentResultInterpreter
 import commanderpepper.labophase.logic.converter.EntryToEntrySelectionUIConverter
 import commanderpepper.labophase.screens.entries.EntrySelectionViewModelImpl
 import commanderpepper.labophase.screens.roundentry.RoundEntryViewModelImpl
 import commanderpepper.labophase.screens.settings.SettingsViewModelImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
-import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val appModule = module {
@@ -23,7 +21,6 @@ val appModule = module {
     single { get<AppDatabase>().entryDao() }
     single<EntryRepository> { EntryRepositoryImpl(get()) }
     single<SettingsRepository> { SettingsRepositoryImpl(androidContext().settingsDataStore, get()) }
-    single { TournamentResultInterpreter }
     single<LeaderOrderDecider> { LeaderOrderDeciderImpl(get(), get()) }
     single { EntryToEntrySelectionUIConverter() }
     viewModel { params -> RoundEntryViewModelImpl(get(), get(), params.getOrNull<Int>()) }
--- app/src/main/java/commanderpepper/labophase/logic/TournamentResultInterpreter.kt
+++
@@ -1,7 +0,0 @@
-package commanderpepper.labophase.logic
-
-object TournamentResultInterpreter {
-    fun transformTournamentResult(result: String): String {
-        return "placeholder"
-    }
-}
